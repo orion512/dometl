ab4ebb73bd77ca5ee44c7ea5afeb5cdf4c25eeb9
Dominik Zulovec Sajovic <dominik.zsajovic@gmail.com>
2022-12-01 02:15:54 +0000

    finalised the game queriy and the game to live query

---
+++ scripts/full_run.sh
@@ -0,0 +1,3 @@
+python -c "from dometl import run_dometl; run_dometl()" -t init -cp dometl_config
+python -c "from dometl import run_dometl; run_dometl()" -t stage -ep datasets\\game_data\\seasons -tb st_game -cp dometl_config
+python -c "from dometl import run_dometl; run_dometl()" -t stage -ep datasets\\url_data\\playoff_urls -tb st_playoff_url -cp dometl_config
